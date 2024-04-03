f4f67db62c2c838de71c01fe659f6f17943758f5
Samuel Southgate <64705405+sSouthgate@users.noreply.github.com>
2024-04-03 15:52:00 +0100

    Start of Project

---
+++ Water_Test.py
@@ -0,0 +1,11 @@
+''' 
+This program aims to be used to perform the experiments required to find the relating equation between soil moistuire, it's delta over time and amount of water distributed.
+    By opening and closing the solenoid valve for a set interval we can find out how much water is distributed in x amount of seconds.
+    This in turn will allow us to calculate the change in moisture overtime when a know amount of water is dispensed.
+    We wil be recording the data and exporting it through a generated .csv file.
+    This file will allow us to plot a graph of the moisture change overtime with a fixed amount water distributed.
+    We will then be able to find the relation between time, water, and moisture, 
+    this will allow us to know how much water is needed to be distributed over how much time to achieve the disired moisture level.
+'''
+print("Initiating Moisture Level Experiment")
+print("Experiment Finished")

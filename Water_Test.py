b8ba545748b968ceecf52fdc5d11546181ac5ea3
Samuel Southgate <64705405+sSouthgate@users.noreply.github.com>
2024-04-03 18:25:16 +0100

    Grove.py folder + Initial code for csv file

--- Water_Test.py
+++ Water_Test.py
@@ -7,5 +7,42 @@ This program aims to be used to perform the experiments required to find the rel
     We will then be able to find the relation between time, water, and moisture, 
     this will allow us to know how much water is needed to be distributed over how much time to achieve the disired moisture level.
 '''
-print("Initiating Moisture Level Experiment")
-print("Experiment Finished")
+# Imports
+import csv
+import os
+from datetime import datetime
+# Variables
+m = 50 #dummy value for moisture
+t = 5 #dummy value for time
+current_date_time = datetime.now()
+T = current_date_time.strftime('%H:%M:%S')
+D = current_date_time.strftime("%d/%m")
+x = 0 #test value
+
+# Set the file path and the headers for the CSV file
+file_path = '../test.csv'
+headers = ["Moisture Level", "Time in s", "Time of Day", "Date"]
+
+# Check if the file exists
+if os.path.exists(file_path):
+    print("File already present, adding to current file")
+if not os.path.exists(file_path):
+    print("File not present, creating file...")
+    # If the file doesn't exist, create it and write the headers to it
+    with open(file_path, 'w', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(headers)
+    print("File created")
+
+print("Writting to CSV File...")
+
+while x < 6:
+    with open('test.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        field = ["Moisture Level", "Time in s", "Time of day"]
+
+        #writer.writerow(field)
+        writer.writerow([m,t,T,D])
+    x += 1
+
+print("Writting finished")
